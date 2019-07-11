8026dd66707f7d40f70f9b1a44ea489cd35768fb
Nikolai Mushegian <hi@nikolai.fyi>
2019-07-11 09:29:40 -0400

    freemap sketch, dh flavored formatting

--- src/dmap.sol
+++ src/dmap.sol
@@ -13,18 +13,26 @@ contract DMap {
         owner = msg.sender;
         emit OwnerUpdate(address(0), owner);
     }
-    function getValue(bytes32 key) public view returns (bytes32) {
+    function getValue(bytes32 key)
+        public view returns (bytes32)
+    {
         return values[key];
     }
-    function setValue(bytes32 key, bytes32 value) public {
+    function setValue(bytes32 key, bytes32 value)
+        public
+    {
         assert(msg.sender == owner);
         values[key] = value;
         emit ValueUpdate(key, value);
     }
-    function getOwner() public view returns (address) {
+    function getOwner()
+        public view returns (address)
+    {
         return owner;
     }
-    function setOwner(address newOwner) public {
+    function setOwner(address newOwner)
+        public
+    {
         assert(msg.sender == owner);
         owner = newOwner;
         emit OwnerUpdate(msg.sender, owner);
