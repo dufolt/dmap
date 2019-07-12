37d309c2d118b8569a88b0d124f1c651cc83bdc3
Nikolai Mushegian <hi@nikolai.fyi>
2019-07-11 21:57:19 -0400

    defer freemap in favor of xreg

--- Makefile
+++ Makefile
@@ -1,1 +1,4 @@
-all:; solc --optimize --pretty-json --combined-json bin,abi src/dmap.sol > dist/dmap.json
+all: dmap xreg
+
+dmap :; solc --optimize --pretty-json --combined-json bin,abi src/dmap.sol > dist/dmap.json
+xreg:; solc --optimize --pretty-json --combined-json bin,abi src/xreg.sol > dist/xreg.json
---
+++ src/xreg.sol
@@ -0,0 +1,24 @@
+// dmap .x.
+// the worst registry
+// do not use this one
+
+pragma solidity ^0.5.10;
+
+import "src/dmap.sol";
+
+contract xreg {
+    DMap public x;
+    constructor() public {
+        x = new DMap();
+        x.setValue(0x0, bytes32(bytes20(address( x ))));
+    }
+    function newChild(bytes32 name) public returns (DMap) {
+        DMap map = new DMap();
+        register(name, bytes32(bytes20(address( x ))));
+        map.setOwner(msg.sender);
+    }
+    function register(bytes32 key, bytes32 val) public {
+        assert(x.getValue(key) == 0x0);
+        x.setValue(key, val);
+    }
+}
